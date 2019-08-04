29b75a4cd1ce3a9139d7412fd1e300c7a433da1b
MalenaLucero <luceromalena@hotmail.com>
2019-08-04 19:32:00 -0300

    dinamically create url

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const url = 'https://kanjiapi.dev/v1/kanji/蛍'
-let kanji
+let kanji = 'nomu'
 
-fetch(url)
+let uri = `https://kanjiapi.dev/v1/reading/
+  ${kanji}/`
+  .replace(/\s/g, '');
+console.log(uri);
+
+
+fetch(uri)
     .then(res=>res.json())
     .then(data=>console.log(data))
     .catch(error=>console.log(error))
@@ -10,4 +16,6 @@ const confirm = () =>{
     let input = document.getElementById('input')
     kanji = input.value
 }
+
+
 
