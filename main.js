db720cb1e5853226ee8aa730c20af76dbf3561f1
MalenaLucero <luceromalena@hotmail.com>
2019-08-05 20:35:04 -0300

    creation of storedWord as object

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
-const url = 'https://kanjiapi.dev/v1/kanji/蛍'
+//const url = 'https://kanjiapi.dev/v1/kanji/蛍'
 let kanji
 let kanjiArray = [] //array with the word's kanji (minus hiragana and katakana)
+let kanjiData = []
+let allWords = []
 
 const confirm = () =>{
     let input = document.getElementById('input')
     kanji = input.value
-    console.log(getKanjiArray(kanji))
     
-
-    kanjiArray.forEach(e=>{
-      let uri = 'https://kanjiapi.dev/v1/kanji/'+e
-      console.log(uri);
-
-      fetch(uri)
+    getKanjiArray(kanji)
+    kanjiArray.forEach((e, index)=>{
+      let url = 'https://kanjiapi.dev/v1/kanji/'+e
+      fetch(url)
           .then(res=>res.json())
-          .then(data=>console.log(data))
+          .then(data=>{
+            let newInternalKanji = new internalKanji(data.kanji, data.jlpt, data.meanings)
+            kanjiArray[index] = newInternalKanji
+            console.log(allWords)
+          })
           .catch(error=>console.log(error))
     })
+
+    let newWord = new storedWord(kanji, kanjiArray)
+    allWords.push(newWord)
+    console.log(allWords)    
 }
 
 //from a full kanji and kana word or phrase, it extracts only the kanji
@@ -24,14 +31,34 @@ const getKanjiArray = (word) =>{
   kanjiArray = []
   //iteration of a string
   for (const c of word) {
-    console.log(c, c.charCodeAt())//unicode in dec
-    console.log(c.charCodeAt().toString(16))//unicode in hex
+    //console.log(c, c.charCodeAt())//unicode in dec
+    //console.log(c.charCodeAt().toString(16))//unicode in hex
     if(c.charCodeAt() >= 13312){
       kanjiArray.push(c)
     }
   }
   return kanjiArray
 }
+
+//object constructor
+function storedWord(word, kanjiList){
+  this.word = word
+  this.kanjiList = kanjiList
+}
+
+//kanji constructor
+function internalKanji(kanji, jlpt, meaning){
+  this.kanji = kanji
+  this.jlpt = jlpt
+  this.meaning = meaning
+}
+
+/*
+word = {
+  'kanji': kanjicompleto,
+  'kanjiArray': []
+}
+*/
 
 
 
