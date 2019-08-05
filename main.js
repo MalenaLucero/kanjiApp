4301c889337cbbfdadf3849ee21b97e8618f7c32
MalenaLucero <luceromalena@hotmail.com>
2019-08-05 15:22:39 -0300

    separated kanji from kana

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 const url = 'https://kanjiapi.dev/v1/kanji/蛍'
-var kanji
+let kanji
+let kanjiArray = []
 
 const confirm = () =>{
     let input = document.getElementById('input')
     kanji = input.value
 
     for (const c of kanji) {
-      console.log(c)
-    }//iteration of a string
+      console.log(c, c.charCodeAt())//unicode in dec
+      let code = c.charCodeAt()
+      console.log(code.toString(16))//unicode in hex
+      if(c.charCodeAt() >= 19968){
+        kanjiArray.push(c)
+      }
+    }//iteration of a string. 
+
+    console.log(kanjiArray)
+
 
 
 
