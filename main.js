8e5cbe47e6da4fa10679705cab9b98056782f105
MalenaLucero <luceromalena@hotmail.com>
2019-08-05 13:34:45 -0300

    dinamic url now works

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 const url = 'https://kanjiapi.dev/v1/kanji/蛍'
-let kanji = 'nomu'
-
-let uri = `https://kanjiapi.dev/v1/reading/
-  ${kanji}/`
-  .replace(/\s/g, '');
-console.log(uri);
-
-
-fetch(uri)
-    .then(res=>res.json())
-    .then(data=>console.log(data))
-    .catch(error=>console.log(error))
+let kanji
 
 const confirm = () =>{
     let input = document.getElementById('input')
     kanji = input.value
+
+    let uri = 'https://kanjiapi.dev/v1/kanji/'+kanji
+    console.log(uri);
+
+    fetch(uri)
+        .then(res=>res.json())
+        .then(data=>console.log(data))
+        .catch(error=>console.log(error))
 }
+
+
+
+
 
 
 
