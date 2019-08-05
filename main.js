ba79b37cb11307fadc5cb5cb57a110d143b17491
MalenaLucero <luceromalena@hotmail.com>
2019-08-05 20:49:38 -0300

    kun readings, on readings and anotations

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 //const url = 'https://kanjiapi.dev/v1/kanji/蛍'
 let kanji
+let anotation
 let kanjiArray = [] //array with the word's kanji (minus hiragana and katakana)
 let kanjiData = []
 let allWords = []
 
 const confirm = () =>{
-    let input = document.getElementById('input')
-    kanji = input.value
+    let kanjiInput = document.getElementById('kanjiInput')
+    kanji = kanjiInput.value
+    let anotationInput = document.getElementById('anotationInput')
+    anotation = anotationInput.value
     
     getKanjiArray(kanji)
     kanjiArray.forEach((e, index)=>{
@@ -14,14 +17,15 @@ const confirm = () =>{
       fetch(url)
           .then(res=>res.json())
           .then(data=>{
-            let newInternalKanji = new internalKanji(data.kanji, data.jlpt, data.meanings)
+            console.log(data)
+            let newInternalKanji = new internalKanji(data.kanji, data.jlpt, data.kun_readings, data.on_readings, data.meanings)
             kanjiArray[index] = newInternalKanji
             console.log(allWords)
           })
           .catch(error=>console.log(error))
     })
 
-    let newWord = new storedWord(kanji, kanjiArray)
+    let newWord = new storedWord(kanji, anotation, kanjiArray)
     allWords.push(newWord)
     console.log(allWords)    
 }
@@ -41,15 +45,18 @@ const getKanjiArray = (word) =>{
 }
 
 //object constructor
-function storedWord(word, kanjiList){
+function storedWord(word, anotation, kanjiList){
   this.word = word
+  this.anotation = anotation
   this.kanjiList = kanjiList
 }
 
 //internal kanji constructor
-function internalKanji(kanji, jlpt, meaning){
+function internalKanji(kanji, jlpt, kun_readings, on_readings, meaning){
   this.kanji = kanji
   this.jlpt = jlpt
+  this.kun_readings = kun_readings
+  this.on_readings = on_readings
   this.meaning = meaning
 }
 
